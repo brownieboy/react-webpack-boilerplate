6f5cecf0b3e6dde10086151b14bfa348ecbe54a8
Michael Brown <brownieboy@gmail.com>
2022-11-06 13:23:27 +1100

    chore: first upload basic demo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>Demo</h2>
+        <p>Hello World!</p>
+      </>
+    );
+  }
+}
+
+export default App;
